ef5c63bd7a6c76f4014f2b0099e99353d2d6d167
ABDUL AHAD LODHI <alodhi2020@gmail.com>
2025-12-06 19:31:57 +0500

    Add files via upload

---
+++ open_ai_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import langchain_openai as OpenAI
+st.title("Quick Start App")
+open_ai_key = st.sidebar.text_input("OpenAI Api Key")
+def generate_response(input_text):
+    llm = OpenAI(temperature = 0.6,open_api_key=open_ai_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area("Enter Your Prompt")
+    submitted = st.form_submit_button("Submit")
+    if not open_ai_key.startswith('sk-'):
+        st.warning("Please Enter Correct OpenAI Api Key")
+    if open_ai_key.startswith('sk-'):
+        generate_response(text)
+    
